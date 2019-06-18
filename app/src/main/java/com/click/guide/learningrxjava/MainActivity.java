b67bf16348f8039c23ba2541b3bde5453760b6e5
jhonjson <qiaosen@laoyuegou.com>
2019-06-18 10:48:28 +0800

    添加另一种实现方式

--- app/src/main/java/com/click/guide/learningrxjava/MainActivity.java
+++ app/src/main/java/com/click/guide/learningrxjava/MainActivity.java
@@ -2,16 +2,23 @@ package com.click.guide.learningrxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AndroidException;
 import android.util.Log;
 
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.Scheduler;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
     Observer<String> observer;
@@ -21,28 +28,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        creatObserver();
-        creatObservable();
+        creatTest1();
+        creatTest2();
     }
 
 
-    // 创建被观察者
-    private void creatObservable() {
-        Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
-            @Override
-            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
-                emitter.onNext("张三来了");
-                emitter.onComplete();
-            }
-        });
-
-        //建立关系
-        observable.subscribe(observer);
-    }
-
-    // 创建观察者
-    private void creatObserver() {
-
+    private void creatTest1() {
+        // 创建观察者
         observer = new Observer<String>() {
             @Override
             public void onSubscribe(Disposable d) {
@@ -64,6 +56,64 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("observer", "observer onComplete");
             }
         };
+
+
+        // 创建被观察者
+        // 第一种方法
+        Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                emitter.onNext("张三来了");
+                emitter.onComplete();
+            }
+        });
+
+        // 第二种方法
+        Observable<String> observable1 = Observable.just("李四来了");
+
+        //建立关系
+        observable.subscribe(observer);
+        observable1.subscribe(observer);
     }
+
+    private void creatTest2() {
+
+        Subscriber subscriber = new Subscriber() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                Log.e("observer", "subscriber onSubscribe");
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Object o) {
+                Log.e("observer", "subscriber onNext " + o.toString());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Log.e("observer", "subscriber onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e("observer", "subscriber onComplete");
+            }
+        };
+
+        Flowable<String> flowable = Flowable.create(new FlowableOnSubscribe<String>() {
+            @Override
+            public void subscribe(FlowableEmitter<String> emitter) throws Exception {
+                emitter.onNext("王五来了");
+                emitter.onComplete();
+            }
+        }, BackpressureStrategy.BUFFER);
+
+        Flowable<String> flowable1 = (Flowable<String>) Flowable.just("赵六来了");
+
+        flowable.subscribe(subscriber);
+        flowable1.subscribe(subscriber);
+    }
+
 
 }
